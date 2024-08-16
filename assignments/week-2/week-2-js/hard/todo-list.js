9d17de46d9b86074bb746b80d7246eff3dfd6c3c
Deepak Kumar <mahtodeepak857@gmail.com>
2024-08-16 15:12:37 +0530

    todo list

--- assignments/week-2/week-2-js/hard/todo-list.js
+++ assignments/week-2/week-2-js/hard/todo-list.js
@@ -11,7 +11,45 @@
 */
 
 class Todo {
+  constructor() {
+    this.listOfTodos = [];
+  }
 
+  add(todo) {
+    this.listOfTodos.push(todo);
+  }
+
+  remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.listOfTodos.length) {
+      this.listOfTodos.splice(indexOfTodo, 1);
+    } else {
+      console.log("Invalid index, nothing removed");
+    }
+  }
+
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.listOfTodos.length) {
+      this.listOfTodos[index] = updatedTodo;
+    } else {
+      console.log("Invalid index, nothing updated");
+    }
+  }
+
+  getAll() {
+    return this.listOfTodos;
+  }
+
+  get(indexOfTodo) {
+    if (this.listOfTodos.length > indexOfTodo) {
+      return this.listOfTodos[indexOfTodo];
+    } else {
+      return null;
+    }
+  }
+
+  clear() {
+    this.listOfTodos = [];
+  }
 }
 
 module.exports = Todo;
