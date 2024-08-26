f90f8b89e851a70fffe96d68ab5e476eb54a0242
Deepak Kumar <mahtodeepak857@gmail.com>
2024-08-26 20:20:52 +0530

    cli to support count_words and count_sentences commands

--- week4-nodejs-http/index.js
+++ week4-nodejs-http/index.js
@@ -27,4 +27,19 @@ program
     });
   });
 
+program
+  .command("count_sentences")
+  .description("Count the number of words in a file")
+  .argument("<file>", "file to count")
+  .action((file) => {
+    fs.readFile(file, "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const lines = data.split("\n").length;
+        console.log(`There are ${lines} sentences in ${file}`);
+      }
+    });
+  });
+
 program.parse();
