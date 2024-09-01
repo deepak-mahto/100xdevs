03e550158180810eb342631b0b703f8a414a91bb
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-01 23:27:30 +0530

    boilerplate

--- week4-nodejs-http/express-offline/index.js
+++ week4-nodejs-http/express-offline/index.js
@@ -1,18 +1,34 @@
-// basic http server
+// real life example
 
 const express = require("express");
-
-function calculateSum(a, b) {
-  return parseInt(a) + parseInt(b);
-}
-
 const app = express();
 
-app.get("/", function (req, res) {
-  const a = req.query.a;
-  const b = req.query.b;
-  const sum = calculateSum(a, b);
-  res.send(sum.toString());
-});
+const users = [
+  {
+    name: "John",
+    kedneys: [
+      {
+        ishealthy: false,
+      },
+      {
+        ishealthy: true,
+      },
+    ],
+  },
+];
+
+// create 4 routes (4 things that the hospital can do)
+
+// GET - User can check how many kidneys they have and their health
+app.get("/", function (req, res) {});
+
+// POST - User can add a new kidney
+app.post("/", function (req, res) {});
+
+// PUT - User can replace a kedney, make it healthy
+app.post("/", function (req, res) {});
+
+// DELETE - User can remove a kidney
+app.post("/", function (req, res) {});
 
 app.listen(3000);
