f057d83f15f3477ffd89eccd153e0c55dfadf6f9
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-04 04:08:00 +0530

    error handling middleware

--- week4-nodejs-http/middlewares-offline/index.js
+++ week4-nodejs-http/middlewares-offline/index.js
@@ -1,14 +1,7 @@
-const request = require("supertest");
-const assert = require("assert");
 const express = require("express");
 
 const app = express();
 let errorCount = 0;
-
-// You have been given an express server which has a few endpoints.
-// Your task is to
-// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
-// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
 app.get("/user", function (req, res) {
   throw new Error("User not found");
@@ -23,4 +16,16 @@ app.get("/errorCount", function (req, res) {
   res.status(200).json({ errorCount });
 });
 
-module.exports = app;
+// You have been given an express server which has a few endpoints.
+// Your task is to
+// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
+// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+
+// error handling middleware
+
+app.use(function (err, req, res, next) {
+  res.status(404).send({});
+  errorCount = errorCount + 1;
+});
+
+app.listen(3000);
