a42bf1ba89e517a100561b8183627ef1a953b061
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-03 17:06:50 +0530

    feat: added the todos in a file

--- week4-nodejs-http/todo-app-express/index.js
+++ week4-nodejs-http/todo-app-express/index.js
@@ -1,36 +1,52 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 app.use(express.json());
 
-let todos = [];
+const filePath = "./todos.json";
+
+// Helper function to read todos from the file
+function readTodosFromFile() {
+  try {
+    const data = fs.readFileSync(filePath, "utf8");
+    return JSON.parse(data);
+  } catch (err) {
+    // If file doesn't exist or is empty, return an empty array
+    return [];
+  }
+}
+
+// Helper function to write todos to the file
+function writeTodosToFile(todos) {
+  fs.writeFileSync(filePath, JSON.stringify(todos, null, 2), "utf8");
+}
 
 app.post("/", function (req, res) {
-  // create a random id for the todo
-  const id = Date.now().toString();
+  const todos = readTodosFromFile();
 
-  // extract the todo title from the body
+  const id = Date.now().toString();
   const title = req.body.title;
+
   if (!title) {
     return res.status(400).send({ message: "Title is required" });
   }
 
-  todos.push({
-    id,
-    title,
-  });
+  todos.push({ id, title });
+  writeTodosToFile(todos);
 
   res.status(201).send({ id, title });
 });
 
 app.delete("/", function (req, res) {
-  // extract the todo id
-  const id = req.body.id;
+  let todos = readTodosFromFile();
 
-  // remove the todo from here
+  const id = req.body.id;
   const index = todos.findIndex((todo) => todo.id === id);
+
   if (index !== -1) {
     todos.splice(index, 1);
+    writeTodosToFile(todos);
     res.status(200).send({ message: "Todo deleted successfully" });
   } else {
     res.status(404).send({ message: "Todo not found" });
@@ -38,9 +54,10 @@ app.delete("/", function (req, res) {
 });
 
 app.get("/", function (req, res) {
-  res.json({
-    todos,
-  });
+  const todos = readTodosFromFile();
+  res.json({ todos });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
