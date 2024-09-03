1703e46eaf1bc3a9da99c51b041f4b4604543e3e
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-04 03:37:43 +0530

    ratelimittler

--- week4-nodejs-http/middlewares-offline/index.js
+++ week4-nodejs-http/middlewares-offline/index.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const app = express();
-let requestCount = 0; // global varibale
 
-// You have been given an express server which have a few endpoints.
-// Your task is to create a global middleware (app.use) which will
-// maintain a count of the number of the requests made to the server in the global
-// requestCount varibale.
+// You have been given an express server which has a few endpoints.
+// Your task is to complete a global middleware (app.use) which will
+// rate limit the requests from a user to only 5 request per second
+// If a user sends more than 5 requests in a single second, the server
+// should block them with a 404.
+// User will be sending in there user id in the header as "user-id"
+// You have been given a numberOfRequestForUser object to start off with which
+// clears every one second
 
-// this function track the number of request have been sent to the server
+let numberOfRequestForUser = {};
+setInterval(function () {
+  numberOfRequestForUser = {};
+}, 1000);
+
 app.use(function (req, res, next) {
-  requestCount = requestCount + 1;
-  next();
+  const userId = req.headers["user-id"];
+  if (numberOfRequestForUser[userId]) {
+    numberOfRequestForUser[userId] = numberOfRequestForUser[userId] + 1;
+    if (numberOfRequestForUser[userId] > 5) {
+      res.status(404).send("no entry");
+    } else {
+      next();
+    }
+  } else {
+    numberOfRequestForUser[userId] = 1;
+    next();
+  }
 }); // global middleware
 
 app.get("/user", function (req, res) {
@@ -19,10 +36,6 @@ app.get("/user", function (req, res) {
 
 app.post("/user", function (req, res) {
   res.status(200).json({ msg: "created dummy user" });
-});
-
-app.get("/requestCount", function (req, res) {
-  res.status(200).json({ requestCount });
 });
 
 app.listen(3000);
