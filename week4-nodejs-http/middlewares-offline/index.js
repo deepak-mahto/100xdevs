6c94c118cbfc0f78d294294e507eff8cd9f871b9
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-04 02:21:45 +0530

    way to track the number of request have been hit to the server

--- week4-nodejs-http/middlewares-offline/index.js
+++ week4-nodejs-http/middlewares-offline/index.js
@@ -1,28 +1,28 @@
 const express = require("express");
 const app = express();
+let requestCount = 0; // global varibale
 
-function isOldEnoughMiddleware(req, res, next) {
-  const age = req.query.age;
-  if (age >= 14) {
-    next();
-  } else {
-    res.status(411).send({ msg: "Sorry you are not of the age yet" });
-  }
-}
+// You have been given an express server which have a few endpoints.
+// Your task is to create a global middleware (app.use) which will
+// maintain a count of the number of the requests made to the server in the global
+// requestCount varibale.
 
-// use middleware at the top of route handlers
-app.use(isOldEnoughMiddleware);
+// this function track the number of request have been sent to the server
+app.use(function (req, res, next) {
+  requestCount = requestCount + 1;
+  next();
+}); // global middleware
 
-app.get("/ride2", function (req, res) {
-  res.status(200).send({
-    msg: "Successfully riden the ride 2",
-  });
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.get("/ride1", function (req, res) {
-  res.status(200).send({
-    msg: "Successfully riden the ride 1",
-  });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
+});
+
+app.get("/user", function (req, res) {
+  res.status(200).json({ requestCount });
 });
 
 app.listen(3000);
