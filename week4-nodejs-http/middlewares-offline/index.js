2645a9ae1ad2602fc186158669a3985203ac4e4f
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-04 02:34:42 +0530

    request count: number of request have been sent to the server

--- week4-nodejs-http/middlewares-offline/index.js
+++ week4-nodejs-http/middlewares-offline/index.js
@@ -21,7 +21,7 @@ app.post("/user", function (req, res) {
   res.status(200).json({ msg: "created dummy user" });
 });
 
-app.get("/user", function (req, res) {
+app.get("/requestCount", function (req, res) {
   res.status(200).json({ requestCount });
 });
 
