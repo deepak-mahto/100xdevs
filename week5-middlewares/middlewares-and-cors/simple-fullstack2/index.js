0bde639e71494bb079e34dd4108c9663dd46a9e8
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-06 01:56:25 +0530

    running frontend and backend in the same url

--- week5-middlewares/middlewares-and-cors/simple-fullstack2/index.js
+++ week5-middlewares/middlewares-and-cors/simple-fullstack2/index.js
@@ -3,15 +3,19 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/sum", function (req, res) {
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/sum", (req, res) => {
   const a = parseInt(req.body.a);
   const b = parseInt(req.body.b);
 
-  res.json({
+  res.status(200).json({
     answer: a + b,
   });
 });
 
 app.listen(3000, () => {
-  console.log("App is listening at port 3000");
+  console.log("Server is listening at port 3000");
 });
