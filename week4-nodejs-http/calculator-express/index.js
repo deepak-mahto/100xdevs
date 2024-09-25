24c712daa752c5daeeae9150f4a127ba5ff2ca84
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-25 18:50:02 +0530

    calculator-backend demo

--- week4-nodejs-http/calculator-express/index.js
+++ week4-nodejs-http/calculator-express/index.js
@@ -12,7 +12,34 @@ app.get("/subtract", function (req, res) {
   const a = parseInt(req.query.a);
   const b = parseInt(req.query.b);
   const subtract = a - b;
-  res.sent(subtract.toString());
+  res.send(subtract.toString());
+});
+
+app.get("/multiply", function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  const multiply = a * b;
+  res.send(multiply.toString());
+});
+
+app.get("/divide", function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  if (b !== 0) {
+    const divide = a / b;
+    res.send(divide.toString());
+  } else {
+    res.json({
+      msg: "can not divide by 0",
+    });
+  }
+});
+
+app.get("/remainder", function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  const remainder = a % b;
+  res.send(remainder.toString());
 });
 
 app.listen(3000);
