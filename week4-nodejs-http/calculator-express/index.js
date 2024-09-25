1a91e5aa3b4a05ca5c57a99741c57e4cbac485ce
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-26 01:37:23 +0530

    removed redundant code

--- week4-nodejs-http/calculator-express/index.js
+++ week4-nodejs-http/calculator-express/index.js
@@ -9,22 +9,22 @@ app.get("/sum", function (req, res) {
 });
 
 app.get("/subtract", function (req, res) {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
+  const a = req.query.a;
+  const b = req.query.b;
   const subtract = a - b;
   res.send(subtract.toString());
 });
 
 app.get("/multiply", function (req, res) {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
+  const a = req.query.a;
+  const b = req.query.b;
   const multiply = a * b;
   res.send(multiply.toString());
 });
 
 app.get("/divide", function (req, res) {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
+  const a = req.query.a;
+  const b = req.query.b;
   if (b !== 0) {
     const divide = a / b;
     res.send(divide.toString());
@@ -36,8 +36,8 @@ app.get("/divide", function (req, res) {
 });
 
 app.get("/remainder", function (req, res) {
-  const a = parseInt(req.query.a);
-  const b = parseInt(req.query.b);
+  const a = req.query.a;
+  const b = req.query.b;
   const remainder = a % b;
   res.send(remainder.toString());
 });
