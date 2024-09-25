57e892fddc3f4e1cf94c96e019d7d89d186c119a
Deepak Kumar <mahtodeepak857@gmail.com>
2024-09-25 18:32:34 +0530

    formatted the code

--- week4-nodejs-http/calculator-express/index.js
+++ week4-nodejs-http/calculator-express/index.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 
-app.get("/sum", function(req, res) {
-	const a = parseInt(req.query.a);
-	const b = parseInt(req.query.b);
-	const sum = a + b;
-	res.send(sum.toString());
-})
+app.get("/sum", function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  const sum = a + b;
+  res.send(sum.toString());
+});
 
-app.get("/subtract", function(req, res){
-	const a = parseInt(req.query.a);
-	const b = parseInt(req.query.b);
-	const subtract = a - b;
-	res.sent(subtract.toString());
-})
+app.get("/subtract", function (req, res) {
+  const a = parseInt(req.query.a);
+  const b = parseInt(req.query.b);
+  const subtract = a - b;
+  res.sent(subtract.toString());
+});
 
 app.listen(3000);
