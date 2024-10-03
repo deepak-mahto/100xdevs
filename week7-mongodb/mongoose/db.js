0799a4aea7e60454569f9cb16e83e9cce68d4e05
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-04 00:58:41 +0530

    Defining Schema and signup endpoint

--- week7-mongodb/mongoose/db.js
+++ week7-mongodb/mongoose/db.js
@@ -1,15 +1,23 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const userId = mongoose.userId;
+const ObjectId = mongoose.ObjectId;
 
 const User = new Schema({
-  email: String,
+  email: { type: String, unique: true },
   password: String,
   name: String,
 });
 
-const Todos = new Schema({
+const Todo = new Schema({
   title: String,
   done: Boolean,
-  userId: userId,
+  userId: ObjectId,
 });
+
+const UserModel = mongoose.Schema("user", User);
+const TodoModel = mongoose.Schema("todos", Todo);
+
+module.exports = {
+  UserModel: UserModel,
+  TodoModel: TodoModel,
+};
--- week7-mongodb/mongoose/index.js
+++ week7-mongodb/mongoose/index.js
@@ -1,7 +1,25 @@
 const express = require("express");
+const { TodoModel, UserModel } = require("./db");
+
 const app = express();
 
-app.post("/signup", function (req, res) {});
+app.use(express.json());
+
+app.post("/signup", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  await UserModel.insert({
+    email: email,
+    password: password,
+    name: name,
+  });
+
+  res.json({
+    message: "You are signed up.",
+  });
+});
 
 app.post("/signin", function (req, res) {});
 
