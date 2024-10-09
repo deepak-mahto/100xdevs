9df89652fc9f7844cca09471f01dc80d89262f97
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-09 18:23:35 +0530

    logInComingRequests

--- assignments/week-4/middlewares/02-logIncomingRequests.js
+++ assignments/week-4/middlewares/02-logIncomingRequests.js
@@ -1,16 +1,22 @@
 //  Create a middleware that logs all incoming requests to the console.
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 function logRequests(req, res, next) {
-    // write the logic for request log here
+  // write the logic for request log here
+  const method = req.method;
+  const url = req.url;
+  const timestamp = new Date().toISOString();
+  console.log(`${method} ${url} - ${timestamp}`);
+
+  next();
 }
 
 app.use(logRequests);
 
-app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello, world!' });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello, world!" });
 });
 
 module.exports = app;
