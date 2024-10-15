d2ac0d3a45013d9a569fd3a27de0fd1d8ed367d5
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-15 23:44:26 +0530

    Added two more buttons decrement and reset

--- week9-react-basics/useState-useEffect/components/Button.jsx
+++ week9-react-basics/useState-useEffect/components/Button.jsx
@@ -6,6 +6,8 @@ const Button = () => {
     <div>
       <h1>{`Count ${count}`}</h1>
       <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 };
