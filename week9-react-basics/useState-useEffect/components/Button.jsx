4cb16cb479001164e3961bdd1b8ce1f7e51c1026
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-16 01:11:52 +0530

    Stop watch v1

--- week9-react-basics/useState-useEffect/components/Button.jsx
+++ week9-react-basics/useState-useEffect/components/Button.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 
+// mounting, re-rendering, unmounting - lifecycle events
 const Button = () => {
   const [count, setCount] = useState(0);
+
+  // hooking into the lifecycle events of react
+  console.log("inside counter component");
+
+  // useEffect will guard the setIntervals from the re-renders
+  useEffect(() => {
+    setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    console.log("mounted");
+  }, []);
+
   return (
     <div>
-      <h1>{`Count ${count}`}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <h1>Stop watch</h1>
+      <h1>{count}</h1>
     </div>
   );
 };
