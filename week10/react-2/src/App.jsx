c1657e5551b499c2b88852413c59db607d1d2a45
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-18 18:47:21 +0530

    Basic routing n spa

--- week10/react-2/src/App.jsx
+++ week10/react-2/src/App.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Link to={"/"}>Allen</Link> |<Link to={"/"}>Class 11</Link> |
+        <Link to={"/"}>Class 12</Link>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path={"/class-11-program"} element={<Class11Program />} />
+          <Route path={"/class-12-program"} element={<Class12Program />} />
+        </Routes>
+        <h2>Allen footer | Contact us</h2>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
+
+function Landing() {
+  return (
+    <>
+      <h1>Welcome to Allen</h1>
+    </>
+  );
+}
+
+function Class11Program() {
+  return (
+    <>
+      <h1>class 11the program</h1>
+    </>
+  );
+}
+
+function Class12Program() {
+  return (
+    <>
+      <h1>class 12th program</h1>
+    </>
+  );
+}
