0f1709b5c2da42664a433cafe3b289afdf4bb52d
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-23 04:35:28 +0530

    use of useEffect

--- c2/6-3/react-recap/src/App.jsx
+++ c2/6-3/react-recap/src/App.jsx
@@ -1,20 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [exchangeData, setExchangeData] = useState({});
   const [bankData, setBankData] = useState({});
 
-  setTimeout(() => {
-    setBankData({
-      income: 100,
-    });
-  }, 1000);
+  useEffect(() => {
+    setTimeout(() => {
+      setBankData({
+        income: 100,
+      });
+    }, 1000);
+  }, []);
 
-  setTimeout(() => {
-    setExchangeData({
-      returns: 100,
-    });
-  }, 1000);
+  useEffect(() => {
+    setTimeout(() => {
+      setExchangeData({
+        returns: 100,
+      });
+    }, 1000);
+  }, []);
 
   const incomeTax = (bankData.income + exchangeData.returns) * 0.3;
   return <div>Your income tax returns are {incomeTax}</div>;
