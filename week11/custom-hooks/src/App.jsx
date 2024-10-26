527a7c067d58745966ffce4d51c5c439e467ebef
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-26 17:54:01 +0530

    Simple counter app

--- week11/custom-hooks/src/App.jsx
+++ week11/custom-hooks/src/App.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 
-function App() {}
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h3>{count}</h3>
+      <button onClick={() => setCount((c) => c + 1)}>Count</button>
+    </div>
+  );
+}
 
 export default App;
