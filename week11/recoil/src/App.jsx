e20011f0f5a05d7b4f526351292003658d88182c
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-28 02:32:27 +0530

    Basic counter app using useState

--- week11/recoil/src/App.jsx
+++ week11/recoil/src/App.jsx
@@ -1,9 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+
 function App() {
   return (
     <div>
-      <h1>
-        <Recoil></Recoil>
-      </h1>
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <CurrentCount count={count} />
+      <Increase setCount={setCount} />
+      <Decrease setCount={setCount} />
+    </div>
+  );
+}
+
+function CurrentCount({ count }) {
+  return (
+    <div>
+      <h1>{count}</h1>
+    </div>
+  );
+}
+
+function Increase({ setCount }) {
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>Increase</button>
+    </div>
+  );
+}
+
+function Decrease({ setCount }) {
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c - 1)}>Decrease</button>
     </div>
   );
 }
