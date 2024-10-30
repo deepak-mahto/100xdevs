0d78ffba6bd2031a4c5c2e0f1daf677a37589b4a
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-30 16:46:47 +0530

    Tailwind

---
+++ c2/8-1/tailwind-prac/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="grid grid-cols-1 w-200 min-h-screen md:grid-cols-3 bg-red-300">
+      <div className="bg-green-500 w-200 h-56 text-center md:w-56">1</div>
+      <div className="bg-red-500 w-200 h-56 text-center md:w-56">2</div>
+      <div className="bg-blue-500 w-200 h-56 text-center md:w-56">3</div>
+    </div>
+  );
+}
+
+export default App;
