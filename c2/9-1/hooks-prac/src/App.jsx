70743ee8b55add20789812597d77ed7c3ccf6caa
Deepak Kumar <mahtodeepak857@gmail.com>
2024-10-31 10:29:04 +0530

    Fetching data from the server

--- c2/9-1/hooks-prac/src/App.jsx
+++ c2/9-1/hooks-prac/src/App.jsx
@@ -1,31 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [render, setRender] = useState(true);
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    setInterval(() => {
-      setRender((r) => !r);
-    }, 5000);
-  }, []);
-
-  return <div>{render ? <MyComponent /> : <h1>From outside the comp</h1>}</div>;
-}
-
-function MyComponent() {
-  useEffect(() => {
-    console.log("component mounted");
-
-    return () => {
-      console.log("component unmounted");
+    const fetchData = async () => {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
+      setTodos(res.data);
     };
+    fetchData();
   }, []);
-
   return (
     <div>
-      <h1>From inside the comp</h1>
+      {todos.map((todo) => (
+        <Track todo={todo} />
+      ))}
+    </div>
+  );
+}
+
+function Track({ todo }) {
+  return (
+    <div>
+      <h2>{todo.title}</h2>
     </div>
   );
 }
