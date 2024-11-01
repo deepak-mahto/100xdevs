dc5355d1cdff2726da7581b61a5f01cbffa9c8e4
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-01 13:16:38 +0530

    Implementation of interface

--- c2/9-2/implementing-interfaces/src/a.ts
+++ c2/9-2/implementing-interfaces/src/a.ts
@@ -19,6 +19,21 @@ class Employee implements Person {
   }
 }
 
-const obj1 = new Employee("deepak", 22);
+class Manager implements Person {
+  name: string;
+  age: number;
 
+  constructor(n: string, a: number) {
+    this.name = n;
+    this.age = a;
+  }
+
+  greet(phrase: string): void {
+    console.log(`${phrase} ${this.name}`);
+  }
+}
+
+const obj1 = new Employee("deepak", 22);
+const obj2 = new Manager("ram", 23);
+console.log(obj2.greet("hi there"));
 console.log(obj1.name);
