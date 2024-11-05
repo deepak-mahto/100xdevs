fddb4e752916068254dfab1e6fded57fae4fdf2e
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-06 03:27:16 +0530

    Init

---
+++ week13-tailwindcss/tailwindcss-1/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="grid grid-cols-3 sm:grid-cols-12">
+      <div className="bg-red-400 col-span-3 sm:col-span-4">hi there 1</div>
+      <div className="bg-blue-300 col-span-3 sm:col-span-4">hi there 2</div>
+      <div className="bg-green-400 col-span-3 sm:col-span-4">hi there 3</div>
+    </div>
+  );
+};
+
+export default App;
