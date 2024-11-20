284d1a57863fdcfe7f76a7a80b9d5210658376d5
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-20 06:49:53 +0530

    types

--- week14-typescript/src/index.ts
+++ week14-typescript/src/index.ts
@@ -1,25 +1,28 @@
-abstract class Person {
+type User = {
   name: string;
   age: number;
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
-  abstract greet(): string;
-  hello() {
-    console.log("hi there");
-  }
-}
+  address?: {
+    city: string;
+    country: string;
+  };
+};
 
-class Employee extends Person {
-  name: string;
-  age: number;
-  constructor(name: string, age: number) {
-    super(name, age);
-    this.name = name;
-    this.age = age;
-  }
-  greet() {
-    return `hi ${this.name}`;
-  }
+const isLegal = (user: User): boolean => {
+  return user.age > 18;
+};
+
+const user: User = {
+  name: "deepak",
+  age: 24,
+  address: {
+    city: "New Delhi",
+    country: "India",
+  },
+};
+const canVote = isLegal(user);
+
+if (canVote) {
+  console.log("can vote");
+} else {
+  console.log("can not vote");
 }
