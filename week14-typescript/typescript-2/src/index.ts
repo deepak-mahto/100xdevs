eb7de42331312675ea7c32c9930d87a630f3d1a1
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-22 12:07:48 +0530

    Add sum of age program

--- week14-typescript/typescript-2/src/index.ts
+++ week14-typescript/typescript-2/src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello World");
+interface User {
+  name: string;
+  age: number;
+}
+const sumOfAge = (user1: User, user2: User) => {
+  return user1.age + user2.age;
+};
+
+const sum = sumOfAge({ name: "deepak", age: 22 }, { name: "ram", age: 23 });
+console.log(sum);
