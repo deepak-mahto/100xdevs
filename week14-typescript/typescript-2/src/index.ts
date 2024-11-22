3cee75211578923ab1153a5aa5b25961439253c4
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-22 15:47:16 +0530

    Exclude

--- week14-typescript/typescript-2/src/index.ts
+++ week14-typescript/typescript-2/src/index.ts
@@ -1,12 +1,9 @@
-type User = {
-  name: string;
-  age: number;
-  email: string;
+type EventType = "click" | "scroll" | "mosemove";
+
+type ExcludeType = Exclude<EventType, "scroll">;
+
+const handleEvent = (event: ExcludeType) => {
+  console.log(`Handling event: ${event}`);
 };
 
-const users = new Map<string, User>();
-users.set("asad12@23", { name: "deepak", age: 23, email: "abd123@gmail.com" });
-users.set("aksfsn12@12", { name: "ram", age: 22, email: "jhg123@gmail.com" });
-
-const user1 = users.get("asad12@23");
-console.log(user1?.email);
+handleEvent("click");
