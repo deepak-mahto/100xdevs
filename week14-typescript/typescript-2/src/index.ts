ca1b0e53a8000b24b1fb6829b6430995de3d6aab
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-22 15:16:26 +0530

    Applying types on Map

--- week14-typescript/typescript-2/src/index.ts
+++ week14-typescript/typescript-2/src/index.ts
@@ -1,6 +1,12 @@
-const users = new Map();
-users.set("asad12@23", { name: "deepak", age: 23 });
-users.set("aksfsn12@12", { name: "ram", age: 22 });
+type User = {
+  name: string;
+  age: number;
+  email: string;
+};
+
+const users = new Map<string, User>();
+users.set("asad12@23", { name: "deepak", age: 23, email: "abd123@gmail.com" });
+users.set("aksfsn12@12", { name: "ram", age: 22, email: "jhg123@gmail.com" });
 
 const user1 = users.get("asad12@23");
-console.log(user1.name);
+console.log(user1?.email);
