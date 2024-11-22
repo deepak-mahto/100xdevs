8eec5fc1447a69c0487cf3af551b5a8af63a4fb2
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-22 13:03:38 +0530

    Partial API

--- week14-typescript/typescript-2/src/index.ts
+++ week14-typescript/typescript-2/src/index.ts
@@ -2,11 +2,14 @@ interface User {
   id: number;
   name: string;
   email: string;
+  password: string;
   createdAt: Date;
 }
 
-type userProfile = Pick<User, "name" | "email">;
+type userProfile = Pick<User, "name" | "email" | "password">;
 
-const displayUserProfile = (user: userProfile) => {
+type updatePropOptional = Partial<userProfile>;
+
+const displayUserProfile = (user: updatePropOptional) => {
   console.log(`Name: ${user.name} Email: ${user.email}`);
 };
