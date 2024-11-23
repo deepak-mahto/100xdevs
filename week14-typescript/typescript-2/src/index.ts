f20078b265e31d62f9a506fdf17293d9a933a237
Deepak Kumar <mahtodeepak857@gmail.com>
2024-11-23 08:22:53 +0530

    Type inferenece in zod

--- week14-typescript/typescript-2/src/index.ts
+++ week14-typescript/typescript-2/src/index.ts
@@ -1,11 +1,31 @@
 import express from "express";
+import { string, z } from "zod";
 
 const app = express();
 const port = 3000;
 
-app.get("/user", (req, res) => {
+const userProfileSchema = z.object({
+  name: z.string().min(1, { message: "Name can not be empty" }),
+  email: z.string().email({ message: "Invalid email format" }),
+  age: z
+    .number()
+    .min(18, { message: "You must be at least 18 years old" })
+    .optional(),
+});
+
+type userProfileType = z.infer<typeof userProfileSchema>;
+
+app.put("/user", (req, res) => {
+  const { success } = userProfileSchema.safeParse(req.body);
+  const updateBody: userProfileType = req.body;
+
+  if (!success) {
+    res.status(411).json({
+      message: "Incorrect inputs",
+    });
+  }
   res.json({
-    message: "Hello",
+    message: "Profile Updated",
   });
 });
 
