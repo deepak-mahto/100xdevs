197c3a848e10c6ebfbc036f8d4230a328dafa9a1
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-02 15:19:46 +0530

    A simple webSocket server

--- week16-websockets/16.1/src/index.ts
+++ week16-websockets/16.1/src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello world");
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (socket) => {
+  console.log("User connected");
+  setInterval(() => {
+    socket.send(`The current price of solana is ${Math.random()}`);
+  }, 500);
+
+  socket.on("message", (e) => {
+    console.log(e.toString());
+  });
+});
