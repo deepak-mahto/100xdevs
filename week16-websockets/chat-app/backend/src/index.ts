fa069636568d0fac86735349aa5bce8a04c67d6c
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-03 02:35:40 +0530

    Broadcasting messages to other user connected on the same websocket server

--- week16-websockets/chat-app/backend/src/index.ts
+++ week16-websockets/chat-app/backend/src/index.ts
@@ -1,15 +1,19 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
 wss.on("connection", (socket) => {
+  allSockets.push(socket);
+
   userCount = userCount + 1;
   console.log(`User connected #${userCount}`);
 
   socket.on("message", (message) => {
     console.log(`Message recieved ${message.toString()}`);
-    setTimeout(() => {
+    for (let i = 0; i < allSockets.length; i++) {
+      const socket = allSockets[i];
       socket.send(`${message.toString()}: sent from the server`);
-    }, 1000);
+    }
   });
 });
