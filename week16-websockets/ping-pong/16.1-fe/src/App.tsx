df638f27eb2567706ced94a4c766a2eb0f88b1f7
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-02 16:02:55 +0530

    INit

---
+++ week16-websockets/ping-pong/16.1-fe/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <input type="text" placeholder="Type message" />
+      <button>Send</button>
+    </>
+  );
+}
+
+export default App;
--- week16-websockets/ping-pong/16.1/src/index.ts
+++ week16-websockets/ping-pong/16.1/src/index.ts
@@ -3,11 +3,10 @@ const wss = new WebSocketServer({ port: 8080 });
 
 wss.on("connection", (socket) => {
   console.log("User connected");
-  setInterval(() => {
-    socket.send(`The current price of solana is ${Math.random()}`);
-  }, 500);
 
   socket.on("message", (e) => {
-    console.log(e.toString());
+    if (e.toString() === "ping") {
+      socket.send("pong");
+    }
   });
 });
