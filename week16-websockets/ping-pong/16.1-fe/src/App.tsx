04c9617f9fa2520ccd2a2fd34eb325249a41f3c4
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-02 16:55:16 +0530

    Ping pong

--- week16-websockets/ping-pong/16.1-fe/src/App.tsx
+++ week16-websockets/ping-pong/16.1-fe/src/App.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useRef, useState } from "react";
+
 function App() {
+  const [socket, setSocket] = useState();
+  const inputRef = useRef();
+
+  function sendMessage() {
+    if (!socket) {
+      return;
+    }
+    // @ts-ignore
+    const message = inputRef.current.value;
+    socket.send(message);
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:8080");
+    setSocket(ws);
+    ws.onmessage = (ev) => {
+      alert(ev.data);
+    };
+  }, []);
   return (
     <>
-      <input type="text" placeholder="Type message" />
-      <button>Send</button>
+      <input ref={inputRef} type="text" placeholder="Type message" />
+      <button onClick={sendMessage}>Send</button>
     </>
   );
 }
