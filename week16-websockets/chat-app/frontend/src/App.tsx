930782f397be78afc8e65647e6b30ce1a9a49739
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-03 20:46:27 +0530

    Frontend for chat-app

--- week16-websockets/chat-app/frontend/src/App.tsx
+++ week16-websockets/chat-app/frontend/src/App.tsx
@@ -1,7 +1,11 @@
 const App = () => {
   return (
-    <div>
-      <h1>Real time chat app</h1>
+    <div className="h-screen bg-black">
+      <div className="h-[95vh]"></div>
+      <div className="flex w-full bg-white">
+        <input type="text" placeholder="Type message" className="flex-1 p-4" />
+        <button className="bg-purple-600 text-white p-4">Submit</button>
+      </div>
     </div>
   );
 };
