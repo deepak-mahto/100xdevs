a155fd201583e39ac83017280e63c50486a14738
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-05 22:54:42 +0530

    Adding data to the database through client

--- week17-postgres/1/src/index.ts
+++ week17-postgres/1/src/index.ts
@@ -1,10 +1,20 @@
 import "dotenv/config";
+import express from "express";
 import { Client } from "pg";
 
+const app = express();
+
 const pgClient = new Client(process.env.DB_CONN_STRING);
+pgClient.connect();
 
-async function main() {
-  await pgClient.connect();
-}
-
-main();
+app.post("/signup", async (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  const response = await pgClient.query(
+    `INSERT INTO users (username, email, password) VALUES (${username}, ${email}, ${password})`
+  );
+  res.json({
+    message: "Sign up successfull",
+  });
+});
