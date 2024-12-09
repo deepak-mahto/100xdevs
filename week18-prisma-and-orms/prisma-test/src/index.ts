34d445e003d5ff34907872f5d3c6b288dd8e736c
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-10 01:21:46 +0530

    To read the user details

--- week18-prisma-and-orms/prisma-test/src/index.ts
+++ week18-prisma-and-orms/prisma-test/src/index.ts
@@ -1,1 +1,26 @@
-console.log("hi there");
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+async function createUser() {
+  await client.user.create({
+    data: {
+      username: "deepak",
+      password: "123123",
+      age: 23,
+      city: "new delhi",
+    },
+  });
+}
+
+async function getUserDetails() {
+  const user = await client.user.findFirst({
+    where: {
+      id: 1,
+    },
+  });
+  return user;
+}
+getUserDetails().then((user) => {
+  console.log(user);
+});
