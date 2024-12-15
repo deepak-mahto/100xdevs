288979e69289ddcedc32cd2e4d3bd9f770fae1a6
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-15 12:27:14 +0530

    children

--- week19-nextjs/app/page.tsx
+++ week19-nextjs/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>learning nextjs</div>;
+  return (
+    <div>
+      <Button>Sign in</Button>
+      <Button>Sign up</Button>
+    </div>
+  );
+}
+
+function Button({ children }) {
+  return <button>{children}</button>;
 }
