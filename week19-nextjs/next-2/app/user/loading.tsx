89ab9290e49a764603c95813c2d5d28135679720
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-17 17:16:55 +0530

    Loading functionality

---
+++ week19-nextjs/next-2/app/user/loading.tsx
@@ -0,0 +1,3 @@
+export default function Loder() {
+  return <div>loading...</div>;
+}
--- week19-nextjs/next-2/app/user/page.tsx
+++ week19-nextjs/next-2/app/user/page.tsx
@@ -4,6 +4,8 @@ export default async function User() {
   const response = await axios.get(
     "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
   );
+  await new Promise((r) => setTimeout(r, 5000));
+
   const data = response.data;
 
   return (
