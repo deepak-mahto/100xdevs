994ef30d0a08804ae9ff54d3deea2dd33de51c35
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-23 18:51:43 +0530

    Catch all routes

--- c2/16.2.2/app/api/auth/[...authRoutes]/route.ts
+++ c2/16.2.2/app/api/auth/[...authRoutes]/route.ts
@@ -1,7 +1,23 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function GET() {
+export function GET(
+  req: NextRequest,
+  {
+    params: { authRoutes },
+  }: {
+    params: {
+      authRoutes: string[];
+    };
+  }
+) {
+  console.log(authRoutes);
   return NextResponse.json({
     message: "hi there",
+  });
+}
+
+export function POST() {
+  return NextResponse.json({
+    message: "this is post route",
   });
 }
