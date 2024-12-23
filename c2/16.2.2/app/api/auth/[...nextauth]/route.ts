d4cbcc812bd66283f4482c775ab5790673d78967
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-23 21:59:36 +0530

    Signin page

---
+++ c2/16.2.2/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "Enter username",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Enter password",
+        },
+      },
+      async authorize(credentials: any) {
+        const username = credentials.username;
+        const password = credentials.password;
+
+        const user = await prisma.user.findFirst({
+          where: {
+            email: username,
+            password: password,
+          },
+        });
+        if (!user) {
+          return null;
+        }
+        return {
+          id: user.id,
+          email: user.email,
+        };
+      },
+    }),
+  ],
+});
+
+export const GET = handler;
+export const POST = handler;
---
+++ c2/16.2.2/app/signup/page.tsx
@@ -0,0 +1,13 @@
+export default function () {
+  return (
+    <div>
+      <div>
+        <input type="text" placeholder="Enter username" />
+      </div>
+      <div>
+        <input type="password" placeholder="Enter password" />
+      </div>
+      <button>Sign up</button>
+    </div>
+  );
+}
