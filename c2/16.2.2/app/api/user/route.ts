7c0fe5fef791216034d03540f8b7e978e1be38c2
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-24 00:29:49 +0530

    User info in an api route

--- c2/16.2.2/app/api/user/route.ts
+++ c2/16.2.2/app/api/user/route.ts
@@ -4,6 +4,6 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const session = await getServerSession();
   return NextResponse.json({
-    session,
+    userInfo: session,
   });
 }
