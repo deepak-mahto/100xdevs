24ddbee733b60df7345cb3ff195e32fa37ee9644
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-24 00:28:38 +0530

    Server component in an api route

--- c2/16.2.2/app/user/page.tsx
+++ c2/16.2.2/app/user/page.tsx
@@ -1,9 +1,11 @@
+import Appbar from "@/components/Appbar";
 import { getServerSession } from "next-auth";
 
 export default async function () {
   const session = await getServerSession();
   return (
     <div>
+      <Appbar />
       <h1>Server component</h1>
       {JSON.stringify(session)}
     </div>
