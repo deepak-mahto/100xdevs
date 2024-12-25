f1e18453bc5c64811f9deda2d49fb73f66b4d4f6
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-26 00:25:55 +0530

    Login and logout functionality

--- week20/next-auth/app/page.tsx
+++ week20/next-auth/app/page.tsx
@@ -1,11 +1,27 @@
-import { useSession } from "next-auth/react";
+"use client";
+import OneLogin from "next-auth/providers/onelogin";
+import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
+  return (
+    <div>
+      <SessionProvider>
+        <RealHome />
+      </SessionProvider>
+    </div>
+  );
+}
+
+function RealHome() {
   const session = useSession();
   return (
     <div>
       next auth
-      {session.status === "authenticated" ? "Logout" : "Login"}
+      {session.status === "authenticated" ? (
+        <button onClick={() => signOut()}>Logout</button>
+      ) : (
+        <button onClick={() => signIn()}>Login</button>
+      )}
     </div>
   );
 }
