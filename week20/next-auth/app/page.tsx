414dd769f24d1b8db81c7caf7e29d7ee14f1e7ab
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-26 01:45:45 +0530

    getServerSession

--- week20/next-auth/app/page.tsx
+++ week20/next-auth/app/page.tsx
@@ -2,10 +2,6 @@ import { getServerSession } from "next-auth";
 
 export default async function Home() {
   const session = await getServerSession();
-  const userProfile = db.avatars.findOne({
-    where: {
-      email: session.email,
-    },
-  });
-  return <div>{JSON.stringify(userProfile.avatar)}</div>;
+
+  return <div>{JSON.stringify(session)}</div>;
 }
