e30d653b7ab2ea82b7c222261845faccaee2863e
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-25 23:26:03 +0530

    Check user on db

--- week20/next-auth/app/api/auth/[...nextauth]/route.ts
+++ week20/next-auth/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,37 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { use } from "react";
 
 const handler = NextAuth({
-  ...
-})
+  providers: [
+    CredentialsProvider({
+      name: "Login with email",
 
-export { handler as GET, handler as POST }
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "jsmith@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const username = credentials?.username;
+        const password = credentials?.password;
+
+        const user = {
+          name: "deepak",
+          id: "1",
+          email: "deepakkumar@gmail.com",
+        };
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
