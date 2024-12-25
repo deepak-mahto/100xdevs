eba371143a97d2a2cd8154a8aa8bc45c89bc8c2a
Deepak Kumar <mahtodeepak857@gmail.com>
2024-12-26 01:11:32 +0530

    useSession

--- week20/next-auth/app/page.tsx
+++ week20/next-auth/app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import OneLogin from "next-auth/providers/onelogin";
 import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
 
 export default function Home() {
@@ -16,10 +15,10 @@ function RealHome() {
   const session = useSession();
   return (
     <div>
-      next auth
-      {session.status === "authenticated" ? (
+      {session.status === "authenticated" && (
         <button onClick={() => signOut()}>Logout</button>
-      ) : (
+      )}
+      {session.status === "unauthenticated" && (
         <button onClick={() => signIn()}>Login</button>
       )}
     </div>
